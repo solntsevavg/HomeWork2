7e0baf06146bf15e5df573d08ce1baf4a3966f57
Виолетта Солнцева <stepviol@live.com>
2025-06-21 13:14:36 +0300

    Добавьте файлы проекта.

---
+++ Task2_1_1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2_1_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       private int _result = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            _result++;
+
+            Text.Text = $"{_result}";
+
+        }
+    
+    }
+}
