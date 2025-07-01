ed7454eae3d3069facbc31d8dc8054e3377b68ae
Виолетта Солнцева <stepviol@live.com>
2025-07-01 22:53:53 +0300

    HW2_2

---
+++ Task2_2_1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2_2_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Open_Click(object sender, RoutedEventArgs e) //Открыть
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox1.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+            StatusText.Text = $"Открыт файл: {openFileDialog.FileName}";
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e) //Сохранить как
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+            }
+            StatusText.Text = $"Файл сохранён: {saveFileDialog.FileName}";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) //Подтверждение выхода из программы при закрытии окна
+        {
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите выйти?", "Подтверждение",
+            MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.ShowDialog();
+        }
+    }
+
+}
