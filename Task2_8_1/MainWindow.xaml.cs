c35d53e99dfc67c3cf1e178d236e73b5bde5ceda
Виолетта Солнцева <stepviol@live.com>
2025-08-10 15:44:35 +0300

    HW2_8

---
+++ Task2_8_1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2_8_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products = new ObservableCollection<Product>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            products.Add(new Product()
+            {
+                ProductName = "Яблоки",
+                Cost = 229.99m,
+                ImageWay = "_avatar/_apple.jpg",
+                ProductType = ProductTypes.Food
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Принтер",
+                Cost = 10999.99m,
+                ImageWay = "_avatar/_printer.jpg",
+                ProductType = ProductTypes.Technic
+            });
+            products.Add(new Product()
+            {
+                ProductName = "Бананы",
+                Cost = 199.49m,
+                ImageWay = "_avatar/_banana.jpg",
+                ProductType = ProductTypes.Food
+            });
+            lstBox.ItemsSource = products;
+        }
+    }
+}
---
+++ Task2_8_1/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task2_8_1
+{
+    public enum ProductTypes
+    {
+        Food,
+        Technic
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public string ImageWay { get; set; }
+
+        public ProductTypes ProductType { get; set; }
+
+    }
+}
