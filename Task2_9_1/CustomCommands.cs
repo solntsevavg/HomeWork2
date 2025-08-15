43f3bf880c98d26c004a582a69ab9049517c5583
Виолетта Солнцева <stepviol@live.com>
2025-08-15 22:22:42 +0300

    HW2-9

---
+++ Task2_9_1/CustomCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task2_9_1
+{
+    internal class CustomCommands
+    {
+        public static RoutedUICommand ChangeColor { get; set; }
+        static CustomCommands()
+        {
+            InputGestureCollection inputGestureCollection = new InputGestureCollection();
+            inputGestureCollection.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+C"));
+            ChangeColor = new RoutedUICommand("Изменить цвет", "Изменить цвет", typeof(CustomCommands), inputGestureCollection);
+        }
+    }
+}
---
+++ Task2_9_1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task2_9_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Коллекция для хранения последовательности цветов
+        private Stack<Brush> _colorHistory = new Stack<Brush>();
+
+        private Brush _currentColor = Brushes.LightGray; //Исходный цвет
+        private Brush _undoColor = Brushes.LightGray;
+
+        // Текущий цвет
+        public Brush CurrentColor
+        {
+            get => _currentColor;
+            set
+            {
+                _undoColor = _currentColor; //Сохраняем предыдущий цвет
+                _currentColor = value;
+
+                // Добавляем новый цвет в стек, если предыдущий цвет не такой же
+                // (либо стек пустой) 
+                if (_colorHistory.Count == 0 || _colorHistory.Peek() != value)
+                {
+                    _colorHistory.Push(value);
+                }
+                // Dock - это имя (x:Name) контейнера компоновки, для которого меняем цвет
+
+                Dock.Background = _currentColor;
+                UndoDock.Background = _undoColor; //Запись предыдущего цвета в нижнюю часть окна
+            }
+        }
+
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var random = new Random();
+            // CurrentColor - это свойство для хранения текущего цвета
+            // ниже описано, как оно устроено
+            CurrentColor = new SolidColorBrush(Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)));
+        }
+
+        private void UndoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_colorHistory.Count > 1)
+            {
+                _colorHistory.Pop(); // Удаляем текущий цвет
+                CurrentColor = _colorHistory.Pop(); // Берём предыдущий
+            }
+        }
+    }
+}
