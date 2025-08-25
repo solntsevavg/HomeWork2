d7c6c998f4961d72498677ebfd8693290f86d9c1
Виолетта Солнцева <stepviol@live.com>
2025-08-25 22:07:04 +0300

    HW2_10

---
+++ Task2_10_1/BoolToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Task2_10_1
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isSuccess)
+                return isSuccess ? Brushes.Green : Brushes.Red;
+
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task2_10_1/Model/AuthModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_10_1.Model
+{
+    static class AuthModel
+    {
+        //Константы для валидных учетных данных
+        public const string ValidUsername = "admin";
+        public const string ValidPassword = "12345";
+
+        static public bool Authenticate(string username, string password)
+        {
+            // Проверка на null или пустые строки
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            return username == ValidUsername && password == ValidPassword;
+
+        }
+    }
+}
