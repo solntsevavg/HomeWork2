75eca574d3b1d795f5774a46433b8aa6fc3e3eb9
Виолетта Солнцева <stepviol@live.com>
2025-09-07 11:34:21 +0300

    корр

--- Task2_9_1/CustomCommands.cs
+++ Task2_9_1/CustomCommands.cs
@@ -12,6 +12,7 @@ namespace Task2_9_1
         public static RoutedUICommand ChangeColor { get; set; }
         static CustomCommands()
         {
+            //Первый параметр в конструкторе - это текст (для него кириллица подходит), а второй - Name (лучше использовать латиницу)
             InputGestureCollection inputGestureCollection = new InputGestureCollection();
             inputGestureCollection.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+C"));
             ChangeColor = new RoutedUICommand("Изменить цвет", "Изменить цвет", typeof(CustomCommands), inputGestureCollection);
